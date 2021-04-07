bcbff512b99845c9a3c095916a5d8492749337f9
Shriyash Balshetwar <52949219+shriyash56@users.noreply.github.com>
2021-04-07 14:02:27 +0530

    Update Header.js

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="flex justify-center font-serif text-5xl font-extrabold bg-green-300">
-            <h1>Recipes</h1>
+            <h1>Hometown Flavors</h1>
         </div>
     )
 }
